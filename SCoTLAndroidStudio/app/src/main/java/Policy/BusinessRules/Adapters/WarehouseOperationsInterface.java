5f0a385743c6eea1123d25c4e3c41b5c44fdbad7
Pedro Santos <pedrosantos.esd@gmail.com>
2023-01-14 15:29:01 -0300

    Apply get warehouses from state name functionality

--- SCoTLAndroidStudio/app/src/main/java/Policy/BusinessRules/Adapters/WarehouseOperationsInterface.java
+++ SCoTLAndroidStudio/app/src/main/java/Policy/BusinessRules/Adapters/WarehouseOperationsInterface.java
@@ -5,7 +5,7 @@ import Policy.Entity.Warehouse;
 public interface WarehouseOperationsInterface 
 {
     public Warehouse[] GetWarehouses();
-    public Warehouse[] GetWarehouses(String stateName);
+    public Warehouse[] GetWarehouses(String stateName, boolean withPastRegister);
     public Warehouse[] GetWarehouses(String stateName, String streetName);
     public Warehouse[] GetWarehousesByOwnerCpf(String ownerCpf);
     public Warehouse[] GetWarehouse(String id, boolean withPastRegister);
