12096bf175457411f05e672595d1021bb170f267
Pedro Santos <pedrosantos.esd@gmail.com>
2023-01-16 07:26:18 -0300

    GetCoffeeBags merge result

--- SCoTLAndroidStudio/app/src/main/java/Frameworks/Database/SQLite/CoffeeBagTableQueryHelper.java
+++ SCoTLAndroidStudio/app/src/main/java/Frameworks/Database/SQLite/CoffeeBagTableQueryHelper.java
@@ -1,16 +1,15 @@
 package Frameworks.Database.SQLite;
 
-<<<<<<< HEAD
+
 import android.database.Cursor;
 
 import Policy.Entity.Batch;
 import Policy.Entity.CoffeeBag;
 import Policy.Entity.Warehouse;
-=======
 import android.content.ContentValues;
 
 import Policy.Entity.CoffeeBag;
->>>>>>> dfeafb485add91e7dd72263c20a78e7f81120132
+
 
 public class CoffeeBagTableQueryHelper
 {
@@ -27,17 +26,12 @@ public class CoffeeBagTableQueryHelper
                 +BATCH_ID+" TEXT,"
                 +WAREHOUSE_ID+" TEXT,"
                 +STORAGE_DATE+" TEXT,"
-<<<<<<< HEAD
                 +"PRIMARY KEY("+ID+", "+BATCH_ID+"),"
-=======
-                +"Primary KEY("+ID+", "+BATCH_ID+"),"
                 +"FOREIGN KEY("+BATCH_ID+") REFERENCES "+BatchTableQueryHelper.BATCH_TABLE+"("+BatchTableQueryHelper.ID+"),"
->>>>>>> dfeafb485add91e7dd72263c20a78e7f81120132
                 +"FOREIGN KEY("+WAREHOUSE_ID+") REFERENCES "+WarehouseTableQueryHelper.WAREHOUSE_TABLE+"("+WarehouseTableQueryHelper.ID+")"
                 +")";
     }
 
-<<<<<<< HEAD
     public static String GetSelectAllQuery()
     {
         return "SELECT * FROM "+COFFEE_BAG_TABLE;
@@ -63,8 +57,7 @@ public class CoffeeBagTableQueryHelper
         return 3;
     }
 
-    public static CoffeeBag GetCoffeeBagFromCoffeeBagCursor(Cursor cursor, Batch batch, Warehouse warehouse)
-    {
+    public static CoffeeBag GetCoffeeBagFromCoffeeBagCursor(Cursor cursor, Batch batch, Warehouse warehouse) {
         return new CoffeeBag
                 (
                         cursor.getString(GetCoffeeBagIdIndex()),
@@ -72,7 +65,7 @@ public class CoffeeBagTableQueryHelper
                         warehouse,
                         cursor.getString(GetStorageDateIndex())
                 );
-=======
+    }
     public static ContentValues GetContentValues(CoffeeBag coffeeBag)
     {
         ContentValues contentValues = new ContentValues();
@@ -81,6 +74,5 @@ public class CoffeeBagTableQueryHelper
         contentValues.put(WAREHOUSE_ID, coffeeBag.GetWarehouse().GetId());
         contentValues.put(STORAGE_DATE, coffeeBag.GetStorageDate());
         return contentValues;
->>>>>>> dfeafb485add91e7dd72263c20a78e7f81120132
     }
 }
