fe72c8f5610d9ff4cb3160ffb7c5b75fddbb6230
Matheus Silva <scruz.math@gmail.com>
2023-01-22 14:44:44 -0300

    Interface de Listar Batch e Cadastrar Batch

--- SCoTLAndroidStudio/app/src/main/java/com/example/scotl/BatchListMenu.java
+++ SCoTLAndroidStudio/app/src/main/java/com/example/scotl/BatchListMenu.java
@@ -2,6 +2,7 @@ package com.example.scotl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -15,20 +16,14 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
-
 import Frameworks.Database.SQLite.SQLiteDAO;
-import Policy.BusinessRules.CRUDBatch;
 import Policy.BusinessRules.DatabaseAccess;
 import Policy.Entity.Batch;
 
 public class BatchListMenu extends AppCompatActivity {
 
 	private TableLayout stk;
-	private EditText id_batch_txt;
-	private EditText creation_date_txt;
+	private EditText date_batch_txt;
 	private Button add_button;
 	private Button list_button;
 
@@ -40,8 +35,7 @@ public class BatchListMenu extends AppCompatActivity {
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		this.setContentView(R.layout.activity_batch_list_menu);
 
-		id_batch_txt = findViewById(R.id.Id_Batch_text);
-		creation_date_txt = findViewById(R.id.creation_Date_text);
+		date_batch_txt = findViewById(R.id.date_Batch_text);
 		add_button = findViewById(R.id.add_button);
 		list_button = findViewById(R.id.list_button);
 		stk = (TableLayout) findViewById(R.id.table_list);
@@ -52,26 +46,7 @@ public class BatchListMenu extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 
-				Batch batch;
-
-				try{
-					batch = new Batch(id_batch_txt.getText().toString(), creation_date_txt.getText().toString());
-					Toast.makeText(BatchListMenu.this, batch.toString(), Toast.LENGTH_SHORT).show();
-
-
-					//Adicionando o novo Lote no banco
-
-					SQLiteDAO database = new SQLiteDAO(BatchListMenu.this);
-					DatabaseAccess.batchOperationsInterface = database;
-					Boolean result = DatabaseAccess.batchOperationsInterface.TryRegisterBatch(batch);
-
-					Toast.makeText(BatchListMenu.this, result.toString(), Toast.LENGTH_SHORT).show();
-
-					Table();
-
-				}catch(Exception e){
-					Toast.makeText(BatchListMenu.this, "Erro ", Toast.LENGTH_SHORT).show();
-				}
+				openActivityCadastrar();
 
 			}
 		});
@@ -80,9 +55,18 @@ public class BatchListMenu extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 
-				stk.removeAllViews();
-				Toast.makeText(BatchListMenu.this, "TESTE2", Toast.LENGTH_SHORT).show();
-				Table();
+				if(date_batch_txt.getText().toString().equals("")){
+					stk.removeAllViews();
+					Table();
+				}
+				else{
+					String date = date_batch_txt.getText().toString();
+					stk.removeAllViews();
+					Table(date);
+					date_batch_txt.setText("");
+
+				}
+
 			}
 		});
 	}
@@ -95,45 +79,36 @@ public class BatchListMenu extends AppCompatActivity {
 		DatabaseAccess.batchOperationsInterface = database;
 		Batch[] batches = DatabaseAccess.batchOperationsInterface.GetBatches();
 
-
-
-
 		TableRow tbrow0 = new TableRow(this);
 
 		TextView tv0 = new TextView(this);
 		tv0.setText(" ID ");
-		tv0.setTextColor(Color.BLACK);
+		tv0.setTextColor(Color.WHITE);
 		tv0.setTextSize(30);
 		tbrow0.addView(tv0);
 
 		TextView tv1 = new TextView(this);
 		tv1.setText(" DATA DE CRIAÇÃO ");
-		tv1.setTextColor(Color.BLACK);
+		tv1.setTextColor(Color.WHITE);
 		tv1.setTextSize(30);
 		tbrow0.addView(tv1);
 
 		stk.addView(tbrow0);
 
-		//Teste para encher a tabela e verificar o scroll
-		Batch[] teste = new Batch[34];
-		for(int i = 0; i < teste.length; i++){
-			teste[i] = new Batch(Integer.toString(i), "18/01/2023");
-		}
-
-		if(teste != null){
+		if(batches != null){
 			for (Batch batch : batches) {
 				TableRow tbrow = new TableRow(this);
 
 				TextView t1v = new TextView(this);
 				t1v.setText(batch.GetId());
-				t1v.setTextColor(Color.parseColor("#FF765138"));
+				t1v.setTextColor(Color.WHITE);
 				t1v.setGravity(Gravity.CENTER);
 				t1v.setTextSize(18);
 				tbrow.addView(t1v);
 
 				TextView t2v = new TextView(this);
 				t2v.setText(batch.GetCreationDate());
-				t2v.setTextColor(Color.parseColor("#FF765138"));
+				t2v.setTextColor(Color.WHITE);
 				t2v.setGravity(Gravity.CENTER);
 				t2v.setTextSize(18);
 				tbrow.addView(t2v);
@@ -143,5 +118,62 @@ public class BatchListMenu extends AppCompatActivity {
 
 		}
 
+	}
+
+	public void Table(String date){
+
+		stk.removeAllViews();
+
+		SQLiteDAO database = new SQLiteDAO(BatchListMenu.this);
+		DatabaseAccess.batchOperationsInterface = database;
+		Batch[] batches = DatabaseAccess.batchOperationsInterface.GetBatches();
+
+
+		TableRow tbrow0 = new TableRow(this);
+
+		TextView tv0 = new TextView(this);
+		tv0.setText(" ID ");
+		tv0.setTextColor(Color.WHITE);
+		tv0.setTextSize(30);
+		tbrow0.addView(tv0);
+
+		TextView tv1 = new TextView(this);
+		tv1.setText(" DATA DE CRIAÇÃO ");
+		tv1.setTextColor(Color.WHITE);
+		tv1.setTextSize(30);
+		tbrow0.addView(tv1);
+
+		stk.addView(tbrow0);
+
+		if(batches != null){
+			for (Batch batch : batches) {
+				if(batch.GetCreationDate().equals(date)) {
+					TableRow tbrow = new TableRow(this);
+
+					TextView t1v = new TextView(this);
+					t1v.setText(batch.GetId());
+					t1v.setTextColor(Color.WHITE);
+					t1v.setGravity(Gravity.CENTER);
+					t1v.setTextSize(18);
+					tbrow.addView(t1v);
+
+					TextView t2v = new TextView(this);
+					t2v.setText(batch.GetCreationDate());
+					t2v.setTextColor(Color.WHITE);
+					t2v.setGravity(Gravity.CENTER);
+					t2v.setTextSize(18);
+					tbrow.addView(t2v);
+
+					stk.addView(tbrow);
+				}
+			}
+
+		}
+
+	}
+
+	public void openActivityCadastrar(){
+		Intent intent = new Intent(this, BatchRegisterMenu.class);
+		startActivity(intent);
 	}
 }
---
+++ SCoTLAndroidStudio/app/src/main/java/com/example/scotl/BatchRegisterMenu.java
@@ -0,0 +1,83 @@
+package com.example.scotl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Frameworks.Database.SQLite.SQLiteDAO;
+import Policy.BusinessRules.DatabaseAccess;
+import Policy.Entity.Batch;
+
+public class BatchRegisterMenu extends AppCompatActivity {
+
+	private EditText id_batch_txt;
+	private EditText creation_date_txt;
+	private Button add_button;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_batch_register_menu);
+
+		id_batch_txt = findViewById(R.id.id_batch_editView);
+		creation_date_txt = findViewById(R.id.creationDate_editView);
+		add_button = findViewById(R.id.cadastrar_button);
+
+		add_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				Batch batch;
+
+				try{
+
+					String id = id_batch_txt.getText().toString();
+					String date = creation_date_txt.getText().toString();
+
+					if(id.equals("")){
+						Toast.makeText(BatchRegisterMenu.this, "ID Lote Vazio", Toast.LENGTH_SHORT).show();
+					}
+					else if(date.equals("")){
+						Toast.makeText(BatchRegisterMenu.this, "Data de Cadastro Vazia", Toast.LENGTH_SHORT).show();
+					}
+					else if(id.length() != 4){
+						Toast.makeText(BatchRegisterMenu.this, "ID Lote necessita de 4 dígitos", Toast.LENGTH_SHORT).show();
+					}
+					else{
+						batch = new Batch(id, date);
+						Toast.makeText(BatchRegisterMenu.this, batch.toString(), Toast.LENGTH_SHORT).show();
+
+						//Adicionando o novo Lote no banco
+
+						SQLiteDAO database = new SQLiteDAO(BatchRegisterMenu.this);
+						DatabaseAccess.batchOperationsInterface = database;
+						Boolean result = DatabaseAccess.batchOperationsInterface.TryRegisterBatch(batch);
+
+						Toast.makeText(BatchRegisterMenu.this, result.toString(), Toast.LENGTH_SHORT).show();
+
+						openActivityBatchList();
+
+					}
+
+				}catch(Exception e){
+					Toast.makeText(BatchRegisterMenu.this, "Erro ", Toast.LENGTH_SHORT).show();
+				}
+
+			}
+		});
+	}
+
+	public void openActivityBatchList(){
+		Intent intent = new Intent(this, BatchListMenu.class);
+		startActivity(intent);
+	}
+}
