720de3a5de2a7b4bf604ccd5127514f8f36a41f3
Matheus Silva <scruz.math@gmail.com>
2023-01-22 15:45:10 -0300

    Atualização CoffeBag Interface

---
+++ SCoTLAndroidStudio/app/src/main/java/com/example/scotl/CoffeeBagInformationMenu.java
@@ -0,0 +1,14 @@
+package com.example.scotl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoffeeBagInformationMenu extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_coffee_bag_information_menu);
+	}
+}
--- SCoTLAndroidStudio/app/src/main/java/com/example/scotl/CoffeeBagMenu.java
+++ SCoTLAndroidStudio/app/src/main/java/com/example/scotl/CoffeeBagMenu.java
@@ -2,6 +2,7 @@ package com.example.scotl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -50,14 +51,14 @@ public class CoffeeBagMenu extends AppCompatActivity {
 		add_button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-
+				openActivityCoffeBagRegister();
 			}
 		});
 
 		list_button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-
+				Table();
 			}
 		});
 	}
@@ -74,26 +75,26 @@ public class CoffeeBagMenu extends AppCompatActivity {
 
 		TextView tv0 = new TextView(this);
 		tv0.setText(" ID SACA ");
-		tv0.setTextColor(Color.BLACK);
-		tv0.setTextSize(20);
+		tv0.setTextColor(Color.WHITE);
+		tv0.setTextSize(16);
 		tbrow0.addView(tv0);
 
 		TextView tv1 = new TextView(this);
 		tv1.setText(" ID LOTE ");
-		tv1.setTextColor(Color.BLACK);
-		tv1.setTextSize(20);
+		tv1.setTextColor(Color.WHITE);
+		tv1.setTextSize(16);
 		tbrow0.addView(tv1);
 
 		TextView tv2 = new TextView(this);
 		tv2.setText(" ID GALPÃO ");
-		tv2.setTextColor(Color.BLACK);
-		tv2.setTextSize(20);
+		tv2.setTextColor(Color.WHITE);
+		tv2.setTextSize(16);
 		tbrow0.addView(tv2);
 
 		TextView tv3 = new TextView(this);
-		tv3.setText(" Dt. DE ARMAZENAMENTO ");
-		tv3.setTextColor(Color.BLACK);
-		tv3.setTextSize(20);
+		tv3.setText(" DATA ARMAZENADO");
+		tv3.setTextColor(Color.WHITE);
+		tv3.setTextSize(16);
 		tbrow0.addView(tv3);
 
 		stk.addView(tbrow0);
@@ -110,28 +111,28 @@ public class CoffeeBagMenu extends AppCompatActivity {
 
 				TextView t1v = new TextView(this);
 				t1v.setText(coffeeBag.GetId());
-				t1v.setTextColor(Color.parseColor("#FF765138"));
+				t1v.setTextColor(Color.WHITE);
 				t1v.setGravity(Gravity.CENTER);
 				t1v.setTextSize(18);
 				tbrow.addView(t1v);
 
 				TextView t2v = new TextView(this);
 				t2v.setText(coffeeBag.GetBatch().GetId());
-				t2v.setTextColor(Color.parseColor("#FF765138"));
+				t2v.setTextColor(Color.WHITE);
 				t2v.setGravity(Gravity.CENTER);
 				t2v.setTextSize(18);
 				tbrow.addView(t2v);
 
 				TextView t3v = new TextView(this);
 				t3v.setText(coffeeBag.GetWarehouse().GetId());
-				t3v.setTextColor(Color.parseColor("#FF765138"));
+				t3v.setTextColor(Color.WHITE);
 				t3v.setGravity(Gravity.CENTER);
 				t3v.setTextSize(18);
 				tbrow.addView(t3v);
 
 				TextView t4v = new TextView(this);
 				t4v.setText(coffeeBag.GetStorageDate());
-				t4v.setTextColor(Color.parseColor("#FF765138"));
+				t4v.setTextColor(Color.WHITE);
 				t4v.setGravity(Gravity.CENTER);
 				t4v.setTextSize(18);
 				tbrow.addView(t4v);
@@ -140,6 +141,11 @@ public class CoffeeBagMenu extends AppCompatActivity {
 			}
 
 		}
+	}
+
+	public void openActivityCoffeBagRegister(){
+		Intent intent = new Intent(this, CoffeeBagRegisterMenu.class);
+		startActivity(intent);
 	}
 
 
